455e8a81a35c95e41f4f63ff1aca0c43918395de
Aditya kumar <aditya7281kumar@gmail.com>
2025-05-19 14:09:28 +0530

    file are update

--- Employee.cpp
+++ Employee.cpp
@@ -1,3 +1,4 @@
+// employee managment system
 #include <iostream>
 #include <vector>
 #include <map>
@@ -8,3 +9,12 @@
 #include <ctime>
 #include <unordered_set>
 #include <unordered_map>
+
+struct Employee{
+    int Employee_ID;
+    std::string Employee_Name;
+    double Salary;
+};
+void Employee_Details(const Employee & Emp){
+    std::cout<<"";
+}
